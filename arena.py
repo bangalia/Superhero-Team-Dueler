b65dfd3f29d025219235a141bcdd17b09806ff73
lotsofluck <anitabangali@yahoo.com>
2020-11-25 18:06:30 -0600

    .

--- arena.py
+++ arena.py
@@ -23,3 +23,100 @@ def create_armor(self):
     name = input("What is this armor called?")
     max_block = input("What is the max_block of this armor?")
     return Armor(name, max_block)
+
+def create_hero(self):
+      hero_name = input("Hero's name: ")
+      hero = Hero(hero_name)
+      add_item = None
+      while add_item != "4":
+         add_item = input("[1] Add ability\n[2] Add weapon\n[3] Add armor\n[4] Done adding items\n\nYour choice: ")
+         if add_item == "1":
+             ability = self.create_ability()
+             hero.add_ability(ability)
+         elif add_item == "2":
+             weapon = self.create_weapon()
+             hero.add_weapon(weapon)
+         elif add_item == "3":
+             armor self.create_armor()
+             hero.add_armor(armor)
+         return hero
+
+def build_team_one(self):
+     numOfTeamMembers = int(input("How many members would you like on Team One?\n"))
+        for i in range(numOfTeamMembers):
+            hero = self.create_hero()
+            self.team_one.add_hero(hero)
+
+def build_team_two(self):
+     numOfTeamMembers = int(input("How many members would you like on Team Two?\n"))
+        for i in range(numOfTeamMembers):
+            hero = self.create_hero()
+            self.team_two.add_hero(hero)
+
+def team_battle(self, team_one, team_two):
+    team_one.attack(team_two)
+
+def show_stats(self):
+        print("\n")
+    print(self.team_one.name + " statistics: ")
+    self.team_one.stats()
+    print("\n")
+    print(self.team_two.name + " statistics: ")
+    self.team_two.stats()
+    print("\n")
+
+    # This is how to calculate the average K/D for Team One
+    team_kills = 0
+    team_deaths = 0
+    for hero in self.team_one.heroes:
+        team_kills += hero.kills
+        team_deaths += hero.deaths
+    if team_deaths == 0:
+        team_deaths = 1
+    print(self.team_one.name + " average K/D was: " + str(team_kills/team_deaths))
+
+    # TODO: Now display the average K/D for Team Two
+    team_kills = 0
+    team_deaths = 0 
+    for hero in self.team_two.heroes:
+        team_kills += hero.kills
+        team_deaths += hero.deaths 
+    if team_deaths == 0:
+        team_deaths = 1
+    print(self.team_two.name + " average K/D was: " + str(team_kills/team_deaths))
+
+
+    # Here is a way to list the heroes from Team One that survived
+    for hero in self.team_one.heroes:
+        if hero.deaths == 0:
+            print("survived from " + self.team_one.name + ": " + hero.name)
+
+    #TODO: Now list the heroes from Team Two that survived
+    for hero in self.team_two.heroes:
+        if hero.deaths == 0:
+            print("survived from " + self.team_two.name + ": " + hero.name)
+
+if __name__ == "__main__":
+    game_is_running = True
+
+    # Instantiate Game Arena
+    arena = Arena()
+
+    #Build Teams
+    arena.build_team_one()
+    arena.build_team_two()
+
+    while game_is_running:
+
+        arena.team_battle()
+        arena.show_stats()
+        play_again = input("Play Again? Y or N: ")
+
+        #Check for Player Input
+        if play_again.lower() == "n":
+            game_is_running = False
+
+        else:
+            #Revive heroes to play again
+            arena.team_one.revive_heroes()
+            arena.team_two.revive_heroes()
